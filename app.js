e5ef4e4087de7226abd02859656017f5a7badf0b
axecution <alkhatibsaed007@gmail.com>
2020-02-20 13:59:38 +0200

    Added multiplications for arrays

--- app.js
+++ app.js
@@ -116,11 +116,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
+    var x = sumAndMultiply(multArr[0],multArr[1],multArr[2]);
+
+   var y =x[1];
+   console.log(y);
+   var msg = `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${y}.`
+   return[y,msg]
 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
